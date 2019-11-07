82a61b7c861ce0e725d62f2cba9811fce046eddd
github username <github email address>
2019-11-06 16:31:14 -0800

    Done.

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,30 @@
+class Ingredient
 
+    @@all = []
+
+    def initialize()
+        @@all << self
+    end
+
+    
+    def self.most_common_allergen
+        hello = Hash.new(0)
+        Allergy.all.each do |ag|
+            hello[ag.ingredient] += 1
+        end
+        max_key = nil
+        max_value = 0
+        hello.each do |key,value|
+            if value > max_value
+                max_value = value 
+                max_key = key
+            end
+        end
+        max_key
+    end
+
+    def self.all 
+        @@all
+    end
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,34 @@
 require_relative '../config/environment.rb'
 
+recipe1 = Recipe.new()
+recipe2 = Recipe.new()
+recipe3 = Recipe.new()
+recipe4 = Recipe.new()
+
+ingredient1 = Ingredient.new()
+ingredient2 = Ingredient.new()
+ingredient3 = Ingredient.new()
+ingredient4 = Ingredient.new()
+
+user1 = User.new()
+user2 = User.new()
+user3 = User.new()
+user4 = User.new()
+
+
+allergy1 = Allergy.new(ingredient1,user1)
+allergy2 = Allergy.new(ingredient2,user1)
+allergy3 = Allergy.new(ingredient3,user1)
+allergy4 = Allergy.new(ingredient3,user3)
+
+recipeingredient1 = RecipeIngredient.new(recipe1, ingredient1)
+recipeingredient2 = RecipeIngredient.new(recipe2, ingredient2)
+recipeingredient3 = RecipeIngredient.new(recipe3, ingredient3)
+recipeingredient4 = RecipeIngredient.new(recipe4, ingredient4)
+
+
+recipecard1 = RecipeCard.new()
+
+
 binding.pry
+0
